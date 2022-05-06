321ed2e7b083ff3e72a3e8d359be3c6389bfe317
Marton Kovacs <narancsjuice@gmail.com>
2022-05-06 11:09:54 +0200

    Add timestamp hovering
    Now the timestamp graph on hover will show a popup
    that accurately tells the date and time of an object
    entering and exiting the camera's view.
    
    GH-9

--- plotting.py
+++ plotting.py
@@ -1,6 +1,16 @@
 from motion_detector import df
 from bokeh.plotting import figure, show, output_file
-from bokeh.models import tickers
+from bokeh.models import tickers, HoverTool, ColumnDataSource
+import pandas
+
+# convert datetime to string
+df["Start"] = pandas.to_datetime(df["Start"])
+df["End"] = pandas.to_datetime(df["End"])
+df["Start_string"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_string"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+# convert DataFrame type
+cds = ColumnDataSource(df)
 
 # create plot
 f = figure(x_axis_type="datetime", height=100, width=500,
@@ -13,8 +23,15 @@ f.xaxis.axis_label = None
 f.yaxis.ticker.desired_num_ticks = 1
 
 # write plot
-quadrant = f.quad(left=df["Start"], right=df["End"],
-                  bottom=0, top=1, color="Orange")
+quadrant = f.quad(left="Start", right="End",
+                  bottom=0, top=1, color="Orange", source=cds)
+
+# graph hover popup data
+hover = HoverTool(tooltips=[("Start", "@Start_string"),
+                            ("End", "@End_string")])
+
+f.add_tools(hover)
+
 # save plot to file
 output_file("motion_graph.html")
 
